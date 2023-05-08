26252cf44832e5cace564712163170a7d82c31a8
ryan4yin <xiaoyin_c@qq.com>
2023-05-08 22:41:45 +0800

    feat: update chrome/vscode's config for fcitx5

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -11,17 +11,42 @@
   in {
   home.packages = with pkgs-stable; [
     firefox-wayland
-
-    # chrome wayland support was broken on nixos-unstable branch, so fallback to stable branch for now
-    # https://github.com/swaywm/sway/issues/7562
-    google-chrome
   ];
 
-  programs.vscode = {
-    enable = true;
-    package = pkgs-stable.vscode;  # use the stable version
+  programs = {
 
-    # let vscode sync and update its configuration & extensions across devices, using github account.
-    # userSettings = {};
+    # source code: https://github.com/nix-community/home-manager/blob/master/modules/programs/chromium.nix
+    google-chrome = {
+      enable = true;
+
+      # chrome wayland support was broken on nixos-unstable branch, so fallback to stable branch for now
+      # https://github.com/swaywm/sway/issues/7562
+      package = pkgs-stable.google-chrome;
+
+      commandLineArgs = [
+        # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
+        # (only supported by chromium/chrome at this time, not electron) 
+        "--gtk-version=4"
+        # make it use text-input-v1, which works for kwin 5.27 and weston
+        # "--enable-wayland-ime"
+
+        # enable hardware acceleration - vulkan api
+        # "--enable-features=Vulkan"
+      ];
+    };
+
+    vscode = {
+      enable = true;
+      # use the stable version
+      package = pkgs-stable.vscode.override {
+        commandLineArgs = [
+          # make it use text-input-v1, which works for kwin 5.27 and weston
+          # "--enable-wayland-ime"
+        ];
+      };
+
+      # let vscode sync and update its configuration & extensions across devices, using github account.
+      # userSettings = {};
+    };
   };
 }
