4511f834af6367ace65b1ab43bb52f94be6ec669
ryan4yin <xiaoyin_c@qq.com>
2023-05-25 00:34:58 +0800

    feat: add supported filesystems - ntfs/fat/...

--- hosts/msi-rtx4090/default.nix
+++ hosts/msi-rtx4090/default.nix
@@ -29,6 +29,15 @@
 
   # Enable binfmt emulation of aarch64-linux, this is required for cross compilation.
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  # supported fil systems, so we can mount any removable disks with these filesystems
+  boot.supportedFilesystems = [ 
+    "ext4"
+    "btrfs"
+    "xfs"
+    #"zfs"
+    "ntfs"
+    "fat" "vfat" "exfat"
+  ];
 
   # Bootloader.
   boot.loader = {
