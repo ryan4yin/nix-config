a622057fb4d72a16fc33145e6b31fe142f7e09a1
ryan4yin <xiaoyin_c@qq.com>
2023-05-31 16:21:35 +0800

    feat: add vscode-insiders to give copilot-x chat a try

--- home/linux/desktop/default.nix
+++ home/linux/desktop/default.nix
@@ -21,5 +21,17 @@
     # remote desktop(rdp connect)
     remmina
     freerdp  # required by remmina
+
+    # the vscode insiders is designed to run alongside the main build, 
+    # with a separate code-insiders command and a different config path
+    ((pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: rec {
+      src = (builtins.fetchTarball {
+        url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
+        # you need to update this sha256 every time you update vscode insiders
+        # the latest sha256 is printed in the error message of `sudo nixos-rebuild switch`
+        sha256 = "sha256:1f996x5i85zf0hpd7jx18zdqdp9nhxhf6zn83ai0njphz1dj354p";
+      });
+      version = "latest";
+    }))
   ];
 }
