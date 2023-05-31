058031ad2902f58d8d3cbc21aeeb477bdb087bda
ryan4yin <xiaoyin_c@qq.com>
2023-05-31 16:28:14 +0800

    feat: add alias - http-proxy

--- home/common/core.nix
+++ home/common/core.nix
@@ -86,10 +86,10 @@
 
   home.shellAliases = {
     k = "kubectl";
+    vim = "nvim";
 
     urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
     urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
-
-    vim = "nvim";
+    httpproxy = "export https_proxy=http://127.0.0.1:7890; export http_proxy=http://127.0.0.1:7890;";
   };
 }
