65b5146dcbfecca3048f4c2d67481f2e3edc7feb
Ryan Yin <xiaoyin_c@qq.com>
2023-07-01 22:46:54 +0800

    feat: enable libvirt(qemu/kvm), install qemu_full(qemu-system-riscv64/qemu-system-loongarch64/...)

--- modules/nixos/libvirt.nix
+++ modules/nixos/libvirt.nix
@@ -15,14 +15,26 @@
       # To fix these, manually change ownership of affected files in /var/lib/libvirt/qemu to qemu-libvirtd.
       qemu.runAsRoot = true;
     };
-
-    qemu = {
-      # default to QEMU/KVM
-      package = pkgs.qemu_kvm;
-    };
   };
   programs.dconf.enable = true;
-  environment.systemPackages = with pkgs; [ virt-manager ];
+  environment.systemPackages = with pkgs; [ 
+    virt-manager
+
+    # QEMU/KVM, provides:
+    #   qemu-storage-daemon qemu-edid qemu-ga
+    #   qemu-pr-helper qemu-nbd elf2dmp qemu-img qemu-io
+    #   qemu-kvm qemu-system-x86_64 qemu-system-aarch64 qemu-system-i386
+    qemu_kvm
+
+    # Install all packages about QEMU, provides:
+    #   ......
+    #   qemu-loongarch64 qemu-system-loongarch64
+    #   qemu-riscv64 qemu-system-riscv64 qemu-riscv32  qemu-system-riscv32
+    #   qemu-system-arm qemu-arm qemu-armeb qemu-system-aarch64 qemu-aarch64 qemu-aarch64_be
+    #   qemu-system-xtensa qemu-xtensa qemu-system-xtensaeb qemu-xtensaeb
+    #   ......
+    qemu_full
+  ];
 
   boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
   # Enable nested virsualization, required by security containers and nested vm.
