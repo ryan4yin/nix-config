678016e3917dbb1dc270a29c5d87bcec28d46df4
Ryan Yin <xiaoyin_c@qq.com>
2023-07-12 11:37:01 +0800

    feat: neovim - add noice.nvim - enhanced ui

--- home/base/desktop/neovim/astronvim_user_init.lua
+++ home/base/desktop/neovim/astronvim_user_init.lua
@@ -33,6 +33,24 @@ return {
         require("catppuccin").setup {}
       end,
     },
+
+    -- Highly experimental plugin that completely replaces the UI for messages, cmdline and the popupmenu. 
+    {
+      "folke/noice.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- add any options here
+      },
+      dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+        }
+    },
+
     {
       "rebelot/heirline.nvim",
       opts = function(_, opts)
