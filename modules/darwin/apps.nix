fa9f23751c01286b91445a300fdadd79273b6017
Ryan Yin <xiaoyin_c@qq.com>
2023-07-15 19:12:48 +0800

    docs: remap capslock key

--- modules/darwin/apps.nix
+++ modules/darwin/apps.nix
@@ -122,8 +122,8 @@
       enableKeyMapping = true;  # enable key mapping so that we can use `option` as `control`
 
       # NOTE: do NOT support remap capslock to both control and escape at the same time
-      remapCapsLockToControl = false;  # remap caps lock to control
-      remapCapsLockToEscape  = true;   # remap caps lock to escape
+      remapCapsLockToControl = false;  # remap caps lock to control, useful for emac users
+      remapCapsLockToEscape  = true;   # remap caps lock to escape, useful for vim users
 
       # swap left command and left alt, 
       # so it matches common keyboard layout: `ctrl | command | alt`
