1ab318b6db14fa95198270bd9a909cc6ae625eab
Ryan Yin <xiaoyin_c@qq.com>
2023-07-16 16:34:34 +0800

    fix: kitty + nushell on macOS

--- home/base/desktop/kitty.nix
+++ home/base/desktop/kitty.nix
@@ -1,17 +1,20 @@
-{ ... }: {
+{ lib, pkgs, ... }: {
   programs.kitty = {
     enable = true;
     theme = "Catppuccin-Mocha";
-    # darwinLaunchOptions = {};
     font = {
-      name = " JetBrainsMono Nerd Font";
+      name = "JetBrainsMono Nerd Font";
+      size = 14;
     };
 
     settings = {
-      background_opacity = "0.92";
+      background_opacity = "0.95";
       macos_option_as_alt = true;  # Option key acts as Alt on macOS
       scrollback_lines = 10000;
       enable_audio_bell = false;
+    } // lib.mkIf pkgs.stdenv.isDarwin {
+      # macOS specific settings, force kitty to use nushell as default shell
+      shell = "/run/current-system/sw/bin/nu";
     };
   };
 }
