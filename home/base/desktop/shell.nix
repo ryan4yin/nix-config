c34911ba26398dbf699eebd782ab1c2b4f671bde
Ryan Yin <xiaoyin_c@qq.com>
2023-07-24 22:37:07 +0800

    fix: skip load alias_for_work if it does not exist

--- home/base/desktop/shell.nix
+++ home/base/desktop/shell.nix
@@ -3,12 +3,21 @@
   programs.bash = {
     # load the alias file for work
     bashrcExtra = ''
-      source /etc/agenix/alias-for-work.bash
+      alias_for_work=/etc/agenix/alias-for-work.bash
+      if [ -f $alias_for_work ]; then
+        . $alias_for_work
+      else
+        echo "No alias file found for work"
+      fi
     '';
   };
 
   programs.nushell = {
     # load the alias file for work
+    # the file must exist, otherwise nushell will complain about it!
+    #
+    # currently, nushell does not support conditional sourcing of files
+    # https://github.com/nushell/nushell/issues/8214
     extraConfig = ''
       source /etc/agenix/alias-for-work.nushell
     '';
