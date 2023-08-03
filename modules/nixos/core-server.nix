c515ea98077612a3ec4576ab5257af26ceed6bc2
Ryan Yin <xiaoyin_c@qq.com>
2023-08-03 23:53:04 +0800

    feat: start dockerd on boot

--- modules/nixos/core-server.nix
+++ modules/nixos/core-server.nix
@@ -103,5 +103,8 @@
 
   virtualisation.docker = {
     enable = true;
+    # start dockerd on boot.
+    # This is required for containers which are created with the `--restart=always` flag to work.
+    enableOnBoot = true;
   };
 }
