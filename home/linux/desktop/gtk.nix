32a6789f44497bf5da42087bf906ba71c42c3b6e
Ryan Yin <xiaoyin_c@qq.com>
2023-09-06 21:53:38 +0800

    feat: adjust font's dpi

--- home/linux/desktop/gtk.nix
+++ home/linux/desktop/gtk.nix
@@ -14,9 +14,9 @@
   # set dpi for 4k monitor
   xresources.properties = {
     # dpi for Xorg's font
-    "Xft.dpi" = 162;
+    "Xft.dpi" = 150;
     # or set a generic dpi
-    "*.dpi" = 162;
+    "*.dpi" = 150;
   };
 
   # gtk's theme settings, generate files: 
