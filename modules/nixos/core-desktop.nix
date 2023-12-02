ef60e7bf91372fc4c7707efa419aa72d094e13f1
Ryan Yin <xiaoyin_c@qq.com>
2023-12-03 01:06:50 +0800

    fix: warning for i3wm - xdg-portal

--- modules/nixos/core-desktop.nix
+++ modules/nixos/core-desktop.nix
@@ -142,6 +142,20 @@
 
   xdg.portal = {
     enable = true;
+
+    config = {
+      common = {
+        # Use xdg-desktop-portal-gtk for every portal interface...
+        default = [
+          "gtk"
+        ];
+        # except for the secret portal, which is handled by gnome-keyring
+        "org.freedesktop.impl.portal.Secret" = [
+          "gnome-keyring"
+        ];
+      };
+    };
+
     # Sets environment variable NIXOS_XDG_OPEN_USE_PORTAL to 1
     # This will make xdg-open use the portal to open programs,
     # which resolves bugs involving programs opening inside FHS envs or with unexpected env vars set from wrappers.
