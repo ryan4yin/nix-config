74cec7dd8e8278a939dc521ef57d243dde969342
Ryan Yin <xiaoyin_c@qq.com>
2023-12-18 00:44:18 +0800

    feat: nix fmt

--- modules/nixos/server.nix
+++ modules/nixos/server.nix
@@ -3,7 +3,6 @@
     ./base/i18n.nix
     ./base/misc.nix
     ./base/networking.nix
-    ./base/peripherals.nix
     ./base/remote-building.nix
     ./base/user-group.nix
     ./base/visualisation.nix
