2cdb0afe489f2378297f9b0bdf224eaf312fa9b4
Ryan Yin <xiaoyin_c@qq.com>
2023-12-28 00:23:11 +0800

    feat: add theme - catppuccin-yazi

--- home/base/server/yazi.nix
+++ home/base/server/yazi.nix
@@ -1,10 +1,13 @@
-{pkgs-unstable, ...}: {
+{pkgs-unstable, nur-ryan4yin, ...}: {
   # terminal file manager
   programs.yazi = {
     enable = true;
     package = pkgs-unstable.yazi;
+    # Changing working directory when exiting Yazi
     enableBashIntegration = true;
     # TODO: nushellIntegration is broken on release-23.11, wait for master's fix to be released
     enableNushellIntegration = false;
   };
+
+  xgd.configFile."yazi/theme.toml".source = "${nur-ryan4yin.cattppuccin-yazi}/mocha.toml";
 }
