6b978b074d311a1c2b1afd71a8fbb079f92f7b01
Ryan Yin <xiaoyin_c@qq.com>
2023-12-29 00:39:36 +0800

    fix: config for foot, add NIX_PATH for compatibility

--- home/linux/desktop/hyprland/values/wayland-apps.nix
+++ home/linux/desktop/hyprland/values/wayland-apps.nix
@@ -1,8 +1,30 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  nur-ryan4yin,
+  ...
+}: {
+  # refer to https://codeberg.org/dnkl/foot/src/branch/master/foot.ini
+  xdg.configFile."foot/foot.ini".text =
+    (builtins.readFile "${nur-ryan4yin.packages.${pkgs.system}.catppuccin-foot}/catppuccin-mocha.conf")
+    + ''
+      term=foot
+      font=JetBrainsMono Nerd Font:size=13
+      dpi-aware=yes
+      shell=${pkgs.bash}/bin/bash --login -c 'nu --login --interactive'
+    '';
+
   programs = {
     # a wayland only terminal emulator
     foot = {
       enable = true;
+      # foot can also be run in a server mode. In this mode, one process hosts multiple windows.
+      # All Wayland communication, VT parsing and rendering is done in the server process.
+      # New windows are opened by running footclient, which remains running until the terminal window is closed.
+      #
+      # Advantages to run foot in server mode including reduced memory footprint and startup time.
+      # The downside is a performance penalty. If one window is very busy with, for example, producing output,
+      # then other windows will suffer. Also, should the server process crash, all windows will be gone.
+      server.enable = false;
     };
 
     # source code: https://github.com/nix-community/home-manager/blob/master/modules/programs/chromium.nix
--- lib/nixosSystem.nix
+++ lib/nixosSystem.nix
@@ -17,7 +17,12 @@ in
         {
           # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
           nix.registry.nixpkgs.flake = nixpkgs;
-          nix.channel.enable = false; # disable nix-channel, we use flakes instead.
+          nix.channel.enable = false; # remove nix-channel related tools & configs, we use flakes instead.
+          
+          # but NIX_PATH is still used by many useful tools, so we set it to the same value as the one used by this flake.
+          # Make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
+          environment.etc."nix/inputs/nixpkgs".source = "${nixpkgs}";
+          environment.sessionVariables.NIX_PATH = nixpkgs.lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
         }
 
         nixos-generators.nixosModules.all-formats
