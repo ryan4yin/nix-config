d393fa707ff1433e8b8a63c8beb51dde1be956b1
Ryan Yin <xiaoyin_c@qq.com>
2023-12-28 13:55:25 +0800

    feat: lazy.nvim + NixOS

--- home/base/desktop/neovim/default.nix
+++ home/base/desktop/neovim/default.nix
@@ -36,6 +36,12 @@
       vimAlias = true;
 
       # currently we use lazy.nvim as neovim's package manager, so comment this one.
+      # Install packages that will compile locally or download FHS binaries via Nix!
+      # and use lazy.nvim's `dir` option to specify the package directory in nix store.
+      # so that these plugins can work on NixOS.
+      #
+      # related project:
+      #  https://github.com/b-src/lazy-nix-helper.nvim
       plugins = with pkgs.vimPlugins; [
         # search all the plugins using https://search.nixos.org/packages
         telescope-fzf-native-nvim
