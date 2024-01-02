a2dbd51b2e5f3ba6b08848d7d77370d4612a4d58
Ryan Yin <xiaoyin_c@qq.com>
2024-01-02 20:07:38 +0800

    feat: emacs - enable format

--- home/base/desktop/editors/emacs/doom/init.el
+++ home/base/desktop/editors/emacs/doom/init.el
@@ -57,7 +57,7 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       ;;(format +onsave)  ; automated prettiness
+       (format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
