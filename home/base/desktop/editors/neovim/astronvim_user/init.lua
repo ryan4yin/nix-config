3f7c0232516f6bffe364d92122f9fb9c3f46e44b
Ryan Yin <xiaoyin_c@qq.com>
2024-01-06 01:08:04 +0800

    feat: neovim - add orgmode

--- home/base/desktop/editors/neovim/astronvim_user/init.lua
+++ home/base/desktop/editors/neovim/astronvim_user/init.lua
@@ -128,7 +128,7 @@ return {
       build = "cargo build --release",
       ft = { "scm", "scheme" },
     },
-    { "Olical/nfnl", ft = "fennel" },
+    { "Olical/nfnl",                                       ft = "fennel" },
     {
       "Olical/conjure",
       ft = { "clojure", "fennel", "python", "scheme" }, -- etc
@@ -158,6 +158,32 @@ return {
       init = function()
         -- Set configuration options here
         vim.g["conjure#debug"] = true
+      end,
+    },
+    {
+      "nvim-orgmode/orgmode",
+      dependencies = {
+        { "nvim-treesitter/nvim-treesitter", lazy = true },
+      },
+      event = "VeryLazy",
+      config = function()
+        -- Load treesitter grammar for org
+        require("orgmode").setup_ts_grammar()
+
+        -- Setup treesitter
+        require("nvim-treesitter.configs").setup({
+          highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = { "org" },
+          },
+          ensure_installed = { "org" },
+        })
+
+        -- Setup orgmode
+        require("orgmode").setup({
+          org_agenda_files = "~/org/**/*",
+          org_default_notes_file = "~/org/refile.org",
+        })
       end,
     },
 
