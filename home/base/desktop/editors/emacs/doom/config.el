0e0ac0237c312e6b40b5e999c72fb56a8a9f39ac
Ryan Yin <xiaoyin_c@qq.com>
2024-01-06 23:07:03 +0800

    feat: emacs(gui) - tranparent backgroud

--- home/base/desktop/editors/emacs/doom/config.el
+++ home/base/desktop/editors/emacs/doom/config.el
@@ -46,6 +46,9 @@
 ;; other doom's official themes:
 ;;   https://github.com/doomemacs/themes
 (setq doom-theme 'doom-dracula) ;; doom-one doom-dracula doom-nord
+;; Transparent Background
+(set-frame-parameter nil 'alpha-background 93) ; For current frame
+(add-to-list 'default-frame-alist '(alpha-background . 93)); For all new frames henceforth
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
