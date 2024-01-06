b79cc2faf44dc736b41e5c7a070e33461a8990b5
Ryan Yin <xiaoyin_c@qq.com>
2024-01-06 20:09:48 +0800

    feat: disable zellij auto exit

--- home/base/server/zellij/default.nix
+++ home/base/server/zellij/default.nix
@@ -6,27 +6,7 @@ in {
   programs.zellij = {
     enable = true;
   };
-  # auto start zellij in nushell
-  programs.nushell.extraConfig = ''
-    # auto start zellij
-    if not "ZELLIJ" in $env {
-      if "ZELLIJ_AUTO_ATTACH" in $env and $env.ZELLIJ_AUTO_ATTACH == "true" {
-        ^zellij attach -c
-      } else {
-        ^zellij
-      }
-
-      # Auto exit the shell session when zellij exit
-      $env.ZELLIJ_AUTO_EXIT = "true" # enable auto exit
-      if "ZELLIJ_AUTO_EXIT" in $env and $env.ZELLIJ_AUTO_EXIT == "true" {
-        exit
-      }
-    }
-  '';
-
   # only works in bash/zsh, not nushell
   home.shellAliases = shellAliases;
   programs.nushell.shellAliases = shellAliases;
-
-  xdg.configFile."zellij/config.kdl".source = ./config.kdl;
 }
