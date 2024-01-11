04a689c5d197ad9d7d3240eb83d7cfb65a48064c
Ryan Yin <xiaoyin_c@qq.com>
2024-01-11 22:09:17 +0800

    feat: emacs - orgmode

--- home/base/desktop/editors/emacs/doom/init.el
+++ home/base/desktop/editors/emacs/doom/init.el
@@ -164,7 +164,7 @@
        (nix +tree-sitter)
                                         ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org              ; organize your plain life in plain text
+       (org +pandoc +present +roam2 +hugo +pretty)              ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
