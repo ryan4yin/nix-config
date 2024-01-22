a6587cfd79b27e3cb1e139d1a8a87b923de44e0c
Ryan Yin <xiaoyin_c@qq.com>
2024-01-22 23:19:36 +0800

    feat: obs - add plugins

--- home/linux/desktop/base/creative.nix
+++ home/linux/desktop/base/creative.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   pkgs-unstable,
+  pkgs-stable,
   nur-ryan4yin,
   ...
 }: {
@@ -27,6 +28,34 @@
 
   programs = {
     # live streaming
-    obs-studio.enable = true;
+    obs-studio = {
+      enable = true;
+      plugins = with pkgs-stable.obs-studio-plugins; [
+        # screen capture
+        wlrobs
+        # obs-ndi
+        obs-vaapi
+        obs-nvfbc
+        obs-teleport
+        # obs-hyperion
+        droidcam-obs
+        obs-vkcapture
+        obs-gstreamer
+        obs-3d-effect
+        input-overlay
+        obs-multi-rtmp
+        obs-source-clone
+        obs-shaderfilter
+        obs-source-record
+        obs-livesplit-one
+        looking-glass-obs
+        obs-vintage-filter
+        obs-command-source
+        obs-move-transition
+        obs-backgroundremoval
+        advanced-scene-switcher
+        obs-pipewire-audio-capture
+      ];
+    };
   };
 }
