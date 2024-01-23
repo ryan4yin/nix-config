360c218344af0a57ac461a4e2e08d81b400840cd
Ryan Yin <xiaoyin_c@qq.com>
2024-01-23 12:00:44 +0800

    docs: gamemode

--- modules/nixos/base/networking.nix
+++ modules/nixos/base/networking.nix
@@ -18,6 +18,7 @@
   # Network discovery, mDNS
   # With this enabled, you can access your machine at <hostname>.local
   # it's more convenient than using the IP address.
+  # https://avahi.org/
   services.avahi = {
     enable = true;
     nssmdns4 = true;
--- modules/nixos/desktop/game/gamemode.nix
+++ modules/nixos/desktop/game/gamemode.nix
@@ -26,6 +26,16 @@
     powerprofilesctl set power-saver
   '';
 in {
+  # Optimise Linux system performance on demand
+  # https://github.com/FeralInteractive/GameMode
+  # https://wiki.archlinux.org/title/Gamemode
+  #
+  # Usage:
+  #   1. For games/launchers which integrate GameMode support:
+  #      https://github.com/FeralInteractive/GameMode#apps-with-gamemode-integration
+  #      simply running the game will automatically activate GameMode.
+  #   2. For others, launching the game through gamemoderun: `gamemoderun ./game`
+  #   3. For steam: `gamemoderun steam-runtime`
   programs.gamemode = {
     enable = true;
     settings = {
