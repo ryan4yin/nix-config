62505e448870356eacb64dc50549edd32f0b158c
Ryan Yin <xiaoyin_c@qq.com>
2024-01-23 13:22:14 +0800

    feat: add croc for file transfering, and age/sops for file encryption

--- home/base/server/core.nix
+++ home/base/server/core.nix
@@ -76,7 +76,12 @@
     nix-output-monitor
 
     # productivity
-    caddy
+    caddy # A webserver with automatic HTTPS via Let's Encrypt(replacement of nginx)
+    croc # File transfer between computers securely and easily
+
+    # security
+    age
+    sops
   ];
 
   programs = {
