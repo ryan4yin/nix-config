d8aeb36b279443c435b4fae2322f983e4b4da83d
Ryan Yin <xiaoyin_c@qq.com>
2024-01-23 19:44:44 +0800

    fix: darwin - gpg: public key decryption failed: No pinentry

--- modules/darwin/security.nix
+++ modules/darwin/security.nix
@@ -1,7 +1,20 @@
 {
+  config,
+  username,
+  ...
+}: let
+  homeDir = config.users.users."${username}".home;
+in {
   # https://github.com/LnL7/nix-darwin/blob/master/modules/programs/gnupg.nix
+  # try `pkill gpg-agent` if you have issues(such as `no pinentry`)
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = false;
+  };
+
+  # enable logs for debugging
+  launchd.user.agents.gnupg-agent.serviceConfig = {
+    StandardErrorPath = "${homeDir}/Library/Logs/gnupg-agent.stderr.log";
+    StandardOutPath = "${homeDir}/Library/Logs/gnupg-agent.stdout.log";
   };
 }
