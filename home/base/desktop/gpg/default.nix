90cd503219b029ff2267522739f08ae9fbc9f62f
Ryan Yin <xiaoyin_c@qq.com>
2024-01-27 17:14:14 +0800

    feat: use gpg only for pass & ssh, make public keys & trust immutable

--- home/base/desktop/gpg/default.nix
+++ home/base/desktop/gpg/default.nix
@@ -10,13 +10,13 @@
     #
     # If set `mutableTrust` to false, the path $GNUPGHOME/trustdb.gpg will be overwritten on each activation.
     # Thus we can only update trsutedb.gpg via home-manager.
-    mutableTrust = true;
+    mutableTrust = false;
 
     # $GNUPGHOME/pubring.kbx stores all the public keys you specified in `programs.gpg.publicKeys` option.
     #
     # If set `mutableKeys` to false, the path $GNUPGHOME/pubring.kbx will become an immutable link to the Nix store, denying modifications.
     # Thus we can only update pubring.kbx via home-manager
-    mutableKeys = true;
+    mutableKeys = false;
     publicKeys = [
       # https://www.gnupg.org/gph/en/manual/x334.html
       # {
