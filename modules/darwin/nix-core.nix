266a190b19010176458c4c1124dad483467dc13c
Ryan Yin <xiaoyin_c@qq.com>
2024-01-28 10:04:17 +0800

    fix: problem disappeared after replacing the determinated nix-installer by the official one

--- modules/darwin/nix-core.nix
+++ modules/darwin/nix-core.nix
@@ -6,20 +6,11 @@
   #  All the configuration options are documented here:
   #    https://daiderd.com/nix-darwin/manual/index.html#sec-options
   #
+  # History Issues:
+  #  1. Fixed by replace the determinated nix-installer by the official one:
+  #     https://github.com/LnL7/nix-darwin/issues/149#issuecomment-1741720259
+  #
   ###################################################################################
-
-  # Fix: https://github.com/LnL7/nix-darwin/issues/149#issuecomment-1741720259
-  # nix is installed via DeterminateSystems's nix-installer.
-  environment.etc."bashrc".knownSha256Hashes = [
-    "6ffdf5a198ffe73fbcd17def767f52093b42b29149d4a3e911b49ebcb9785101" # nix-installer on fern
-  ];
-  environment.etc."zshenv".knownSha256Hashes = [
-    "0c544e42afe7836de9ba933d93f46043b12f58ae484ff8cfb02716353f1dba5f" # nix-installer on fern
-  ];
-
-  environment.etc."shells".knownSha256Hashes = [
-    "9d5aa72f807091b481820d12e693093293ba33c73854909ad7b0fb192c2db193" # nix-installer on fern
-  ];
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
