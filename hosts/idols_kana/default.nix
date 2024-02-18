1a3b02a0625331795271bc7def8e170aa62337fd
Ryan Yin <xiaoyin_c@qq.com>
2024-02-18 13:41:39 +0800

    feat: remove emulatedSystem from kana & ruby

--- hosts/idols_kana/default.nix
+++ hosts/idols_kana/default.nix
@@ -14,8 +14,6 @@ let
 in {
   imports = mylib.scanPaths ./.;
 
-  # Enable binfmt emulation of aarch64-linux, this is required for cross compilation.
-  boot.binfmt.emulatedSystems = ["aarch64-linux" "riscv64-linux"];
   # supported file systems, so we can mount any removable disks with these filesystems
   boot.supportedFilesystems = [
     "ext4"
