36f5367a5d917f0e2b0343acc84238958f3de549
Ryan Yin <xiaoyin_c@qq.com>
2024-02-18 20:13:34 +0800

    feat: k3s - kubeconfig

--- hosts/k8s/k3s_prod_1_master_1/k3s.nix
+++ hosts/k8s/k3s_prod_1_master_1/k3s.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  username,
   ...
 }: let
   package = pkgs.k3s_1_29;
@@ -28,11 +29,11 @@ in {
     tokenFile = config.age.secrets."k3s-prod-1-token".path;
     # https://docs.k3s.io/cli/server
     extraFlags =
-      " --write-kubeconfig /etc/k3s/kubeconfig.yml"
+      " --write-kubeconfig /home/${username}/.kube/config"
       + " --write-kubeconfig-mode 644"
       + " --service-node-port-range 80-32767"
       + " --data-dir /var/lib/rancher/k3s"
       + " --etcd-expose-metrics true"
-      + '' --etcd-snapshot-schedule-cron "0 */12 * * *"'';
+      + ''--etcd-snapshot-schedule-cron "0 */12 * * *"'';
   };
 }
