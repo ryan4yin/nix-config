bbe0f294355beda2bb5732d84dc455b4c6a7638b
Ryan Yin <xiaoyin_c@qq.com>
2024-02-22 16:14:07 +0800

    feat: adjust wayland related settings for chromium & vscode

--- home/linux/desktop/hyprland/values/wayland-apps.nix
+++ home/linux/desktop/hyprland/values/wayland-apps.nix
@@ -39,12 +39,13 @@
     google-chrome = {
       enable = true;
 
+      # https://wiki.archlinux.org/title/Chromium#Native_Wayland_support
       commandLineArgs = [
+        "--ozone-platform-hint=auto"
+        "--ozone-platform=wayland"
         # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
         # (only supported by chromium/chrome at this time, not electron)
         "--gtk-version=4"
-        "--enable-features=UseOzonePlatform"
-        "--ozone-platform=wayland"
         # make it use text-input-v1, which works for kwin 5.27 and weston
         "--enable-wayland-ime"
 
@@ -67,12 +68,16 @@
         (pkgs.vscode.override
           {
             isInsiders = true;
+            # https://wiki.archlinux.org/title/Wayland#Electron
             commandLineArgs = [
-              # "--disable-gpu"
-              # make it use text-input-v1, which works for kwin 5.27 and weston
-              "--enable-features=UseOzonePlatform"
+              "--ozone-platform-hint=auto"
               "--ozone-platform=wayland"
+              # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
+              # (only supported by chromium/chrome at this time, not electron)
+              "--gtk-version=4"
+              # make it use text-input-v1, which works for kwin 5.27 and weston
               "--enable-wayland-ime"
+
               # TODO: fix https://github.com/microsoft/vscode/issues/187436
               # still not works...
               "--password-store=gnome" # use gnome-keyring as password store
