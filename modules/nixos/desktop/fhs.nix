69a64b209ae5391a66c8c4dc06211bce2cc2b3ac
Ryan Yin <xiaoyin_c@qq.com>
2024-03-04 01:06:13 +0800

    refactor: packages & fhs

---
+++ modules/nixos/desktop/fhs.nix
@@ -0,0 +1,19 @@
+{pkgs, ...}: {
+  # FHS environment, flatpak, appImage, etc.
+  environment.systemPackages = [
+    # create a fhs environment by command `fhs`, so we can run non-nixos packages in nixos!
+    (
+      let
+        base = pkgs.appimageTools.defaultFhsEnvArgs;
+      in
+        pkgs.buildFHSUserEnv (base
+          // {
+            name = "fhs";
+            targetPkgs = pkgs: (base.targetPkgs pkgs) ++ [pkgs.pkg-config];
+            profile = "export FHS=1";
+            runScript = "bash";
+            extraOutputsToInstall = ["dev"];
+          })
+    )
+  ];
+}
