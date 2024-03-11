5af7c7a4275490a9a7d89a88f5524094714645c0
Ryan Yin <xiaoyin_c@qq.com>
2024-03-11 11:56:33 +0800

    fix: aarch64-darwin - fern

--- home/base/desktop/editors/packages.nix
+++ home/base/desktop/editors/packages.nix
@@ -125,7 +125,7 @@
       hadolint # Dockerfile linter
     ]
     ++ (
-      lib.optionals (pkgs.system != "x86_64-darwin") [
+      lib.optionals pkgs.stdenv.isLinux [
         jsonnet
         #-- zig
         zls
