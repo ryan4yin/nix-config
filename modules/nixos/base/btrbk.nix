8bc96ba0e8624b143658578f95eb4fd9edaa84ec
Ryan Yin <xiaoyin_c@qq.com>
2024-03-31 23:06:53 +0800

    docs: usage of btrbk

--- modules/nixos/base/btrbk.nix
+++ modules/nixos/base/btrbk.nix
@@ -1,6 +1,21 @@
 {
+  # ==================================================================
+  #
   # Tool for creating snapshots and remote backups of btrfs subvolumes
-  # https://github.com/digint/btrbk
+  #   https://github.com/digint/btrbk
+  #
+  # Usage:
+  #   1. btrbk will create snapshots on schedule
+  #   2. we can use `btrbk run` command to create a backup manually
+  #
+  # How to restore a snapshot:
+  #   1. Find the snapshot you want to restore in /snapshots
+  #   2. Use `btrfs subvol delete /btr_pool/@persistent` to delete the current subvolume
+  #   3. Use `btrfs subvol snapshot /snapshots/2021-01-01 /btr_pool/@persistent` to restore the snapshot
+  #   4. reboot the system or remount the filesystem to see the changes
+  #
+  # ==================================================================
+
   services.btrbk.instances.btrbk = {
     # How often this btrbk instance is started. See systemd.time(7) for more information about the format.
     onCalendar = "daily";
