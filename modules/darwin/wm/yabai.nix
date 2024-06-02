54e4dfcec0598f0554dc73936214eb28d7c6f455
Ryan Yin <xiaoyin_c@qq.com>
2024-06-02 13:35:40 +0800

    chore: update yabai

--- modules/darwin/wm/yabai.nix
+++ modules/darwin/wm/yabai.nix
@@ -1,34 +1,13 @@
 {
-  pkgs,
   config,
-  lib,
   myvars,
-  pkgs-unstable,
   ...
 }: let
   homeDir = config.users.users."${myvars.username}".home;
 in {
-  # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/os-specific/darwin/yabai/default.nix
+  # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/ya/yabai/package.nix
   services.yabai = {
     enable = true;
-    # temporary workaround for https://github.com/ryan4yin/nix-config/issues/51
-    package = pkgs-unstable.yabai.overrideAttrs (oldAttrs: rec {
-      version = "6.0.7";
-      src =
-        if pkgs.stdenv.isAarch64
-        then
-          (pkgs.fetchzip {
-            url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
-            hash = "sha256-hZMBXSCiTlx/37jt2yLquCQ8AZ2LS3heIFPKolLub1c=";
-          })
-        else
-          (pkgs.fetchFromGitHub {
-            owner = "koekeishiya";
-            repo = "yabai";
-            rev = "v${version}";
-            hash = "sha256-vWL2KA+Rhj78I2J1kGItJK+OdvhVo1ts0NoOHIK65Hg=";
-          });
-    });
 
     # Whether to enable yabai's scripting-addition.
     # SIP must be disabled for this to work.
