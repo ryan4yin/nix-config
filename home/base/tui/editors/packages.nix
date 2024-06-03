0f4387800f62d18b7d87f9c4511dfaf720e96cb2
Ryan Yin <xiaoyin_c@qq.com>
2024-06-03 10:11:17 +0800

    fix: #122 - cgo-builtin-prolog:1:10: fatal error: 'stddef.h' file not found

--- home/base/tui/editors/packages.nix
+++ home/base/tui/editors/packages.nix
@@ -15,7 +15,8 @@
     gcc
     # c/c++ tools with clang-tools, the unwrapped version won't
     # add alias like `cc` and `c++`, so that it won't conflict with gcc
-    llvmPackages.clang-unwrapped
+    # llvmPackages.clang-unwrapped
+    clang-tools
     lldb
 
     #-- python
