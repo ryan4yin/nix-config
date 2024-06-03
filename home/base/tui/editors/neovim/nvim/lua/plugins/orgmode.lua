21555a4148842a22b34ce91edd869884e37b00cb
Ryan Yin <xiaoyin_c@qq.com>
2024-06-03 14:33:39 +0800

    feat(neovim): update plugins - orgmode

--- home/base/tui/editors/neovim/nvim/lua/plugins/orgmode.lua
+++ home/base/tui/editors/neovim/nvim/lua/plugins/orgmode.lua
@@ -1,26 +1,19 @@
 return {
-  "nvim-orgmode/orgmode",
-  dependencies = {
-    { "nvim-treesitter/nvim-treesitter", lazy = true },
-  },
-  event = "VeryLazy",
+  'nvim-orgmode/orgmode',
+  event = 'VeryLazy',
+  ft = { 'org' },
   config = function()
-    -- Load treesitter grammar for org
-    require("orgmode").setup_ts_grammar()
-
-    -- Setup treesitter
-    require("nvim-treesitter.configs").setup {
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { "org" },
-      },
-      ensure_installed = { "org" },
-    }
-
     -- Setup orgmode
-    require("orgmode").setup {
+    require('orgmode').setup({
       org_agenda_files = "~/org/**/*",
       org_default_notes_file = "~/org/refile.org",
-    }
+    })
+
+    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+    -- add ~org~ to ignore_install
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = 'all',
+      ignore_install = { 'org' },
+    })
   end,
 }
