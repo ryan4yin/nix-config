4433e018d9cb4975ab4279dcf188b5a48180b90a
Ryan Yin <xiaoyin_c@qq.com>
2024-06-16 21:38:14 +0800

    feat: disable treesitter-wing

--- home/base/tui/editors/neovim/nvim/lua/plugins/treesitter.lua
+++ home/base/tui/editors/neovim/nvim/lua/plugins/treesitter.lua
@@ -17,7 +17,7 @@ return {
         node_decremental = "<bs>", -- Backspace
       },
     }
-    opts.ignore_install = { "gotmpl" }
+    opts.ignore_install = { "gotmpl", "wing" }
 
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
