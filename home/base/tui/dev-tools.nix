bcf5b8b1cb589d81555b42488c830e13c38daa97
Ryan Yin <xiaoyin_c@qq.com>
2024-06-24 09:11:43 +0800

    feat: add gitleaks

--- home/base/tui/dev-tools.nix
+++ home/base/tui/dev-tools.nix
@@ -41,6 +41,7 @@
     # Automatically trims your branches whose tracking remote refs are merged or gone
     # It's really useful when you work on a project for a long time.
     git-trim
+    gitleaks
 
     # need to run `conda-install` before using it
     # need to run `conda-shell` before using command `conda`
