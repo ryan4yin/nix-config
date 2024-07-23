d8759cc8458b0217daf06fce7db25ca578aaae38
Ryan Yin <xiaoyin_c@qq.com>
2024-07-23 09:49:04 +0800

    feat: darwin - font.packages

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -185,13 +185,7 @@
 
   # Fonts
   fonts = {
-    # will be removed after this PR is merged:
-    #   https://github.com/LnL7/nix-darwin/pull/754
-    fontDir.enable = true;
-
-    # will change to `fonts.packages` after this PR is merged:
-    #   https://github.com/LnL7/nix-darwin/pull/754
-    fonts = with pkgs; [
+    packages = with pkgs; [
       # packages = with pkgs; [
       # icon fonts
       material-design-icons
