fed3bc981b088003acf3a991870a868253703795
Ryan Yin <xiaoyin_c@qq.com>
2024-07-29 23:42:14 +0800

    fix: k3s test cluster - token path

--- hosts/k8s/k3s-test-1-master-1/default.nix
+++ hosts/k8s/k3s-test-1-master-1/default.nix
@@ -14,7 +14,7 @@
   k3sModule = mylib.genK3sServerModule {
     inherit pkgs;
     kubeconfigFile = "/home/${myvars.username}/.kube/config";
-    tokenFile = config.age.secrets."k3s-prod-1-token".path;
+    tokenFile = config.age.secrets."k3s-test-1-token".path;
     # the first node in the cluster should be the one to initialize the cluster
     clusterInit = true;
     # use my own domain & kube-vip's virtual IP for the API server
--- hosts/k8s/k3s-test-1-master-3/default.nix
+++ hosts/k8s/k3s-test-1-master-3/default.nix
@@ -14,7 +14,7 @@
   k3sModule = mylib.genK3sServerModule {
     inherit pkgs;
     kubeconfigFile = "/home/${myvars.username}/.kube/config";
-    tokenFile = config.age.secrets."k3s-prod-1-token".path;
+    tokenFile = config.age.secrets."k3s-test-1-token".path;
     # use my own domain & kube-vip's virtual IP for the API server
     # so that the API server can always be accessed even if some nodes are down
     masterHost = "test-cluster-1.writefor.fun";
