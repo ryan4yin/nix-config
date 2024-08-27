08895478a42f71a3bd5b8f94e034835e13c40cde
Ryan Yin <xiaoyin_c@qq.com>
2024-08-27 23:46:25 +0800

    feat(darwin): auto upgrade homebrew

--- modules/darwin/apps.nix
+++ modules/darwin/apps.nix
@@ -90,7 +90,8 @@ in {
     enable = true; # disable homebrew for fast deploy
 
     onActivation = {
-      autoUpdate = true;
+      autoUpdate = true; # Fetch the newest stable branch of Homebrew's git repo
+      upgrade = true; # Upgrade outdated casks, formulae, and App Store apps
       # 'zap': uninstalls all formulae(and related files) not listed in the generated Brewfile
       cleanup = "zap";
     };
