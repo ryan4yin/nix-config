cca62222ef295da846fbbff3a8c538b5791797e6
Ryan Yin <xiaoyin_c@qq.com>
2024-08-29 11:11:04 +0800

    feat(home/base/tui/edistors/neovim): adjust mason.nvim

--- home/base/tui/editors/neovim/nvim/lua/plugins/mason.lua
+++ home/base/tui/editors/neovim/nvim/lua/plugins/mason.lua
@@ -67,9 +67,6 @@ return {
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
-      -- The directory in which to install packages.
-      opts.install_root_dir = path.concat { vim.fn.stdpath "data", "mason" }
-
       -- Where Mason should put its bin location in your PATH. Can be one of:
       -- - "prepend" (default, Mason's bin location is put first in PATH)
       -- - "append" (Mason's bin location is put at the end of PATH)
