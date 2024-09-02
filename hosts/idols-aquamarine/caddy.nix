f87faf97d6b0e14a4212b9a5f731167d4fe159cb
Ryan Yin <xiaoyin_c@qq.com>
2024-09-02 18:00:31 +0800

    fix: minio's reverse proxy

--- hosts/idols-aquamarine/caddy.nix
+++ hosts/idols-aquamarine/caddy.nix
@@ -94,12 +94,36 @@ in {
     virtualHosts."minio.writefor.fun".extraConfig = ''
       ${hostCommonConfig}
       encode zstd gzip
-      reverse_proxy http://localhost:9096
+      reverse_proxy http://localhost:9096 {
+        header_up Host {http.request.host}
+        header_up X-Real-IP {http.request.remote.host}
+        header_up X-Forwarded-For {http.request.header.X-Forwarded-For}
+        header_up X-Forwarded-Proto {scheme}
+        transport http {
+            versions h2c
+            dial_timeout 300s
+            read_timeout 300s
+            write_timeout 300s
+        }
+      }
     '';
     virtualHosts."minio-ui.writefor.fun".extraConfig = ''
       ${hostCommonConfig}
       encode zstd gzip
-      reverse_proxy http://localhost:9097
+      reverse_proxy http://localhost:9097 {
+        header_up Host {http.request.host}
+        header_up X-Real-IP {http.request.remote.host}
+        header_up X-Forwarded-For {http.request.header.X-Forwarded-For}
+        header_up X-Forwarded-Proto {scheme}
+        header_up Upgrade {http.request.header.Upgrade}
+        header_up Connection {http.request.header.Connection}
+        transport http {
+            versions h2c
+            dial_timeout 300s
+            read_timeout 300s
+            write_timeout 300s
+        }
+      }
     '';
   };
   networking.firewall.allowedTCPPorts = [80 443];
