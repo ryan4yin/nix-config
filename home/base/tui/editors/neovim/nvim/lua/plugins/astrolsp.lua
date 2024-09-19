5f42a1442e8c796b9b4cd5b30bc6d36ceedaa2f6
Ryan Yin <xiaoyin_c@qq.com>
2024-09-19 08:33:00 +0800

    feat(neovim): enable inlay_hints - neovim v0.10+

--- home/base/tui/editors/neovim/nvim/lua/plugins/astrolsp.lua
+++ home/base/tui/editors/neovim/nvim/lua/plugins/astrolsp.lua
@@ -12,7 +12,7 @@ return {
     features = {
       autoformat = true, -- enable or disable auto formatting on start
       codelens = true, -- enable/disable codelens refresh on start
-      inlay_hints = false, -- enable/disable inlay hints on start
+      inlay_hints = true, -- enable/disable inlay hints on start
       semantic_tokens = true, -- enable/disable semantic token highlighting
     },
     -- customize lsp formatting options
