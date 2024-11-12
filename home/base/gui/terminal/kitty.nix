62493fb410393e7c75dee5705b667a226b9a8279
Ryan Yin <xiaoyin_c@qq.com>
2024-11-12 21:20:36 +0800

    chore(kitty): update deprecated option

--- home/base/gui/terminal/kitty.nix
+++ home/base/gui/terminal/kitty.nix
@@ -19,7 +19,7 @@
     # kitty has catppuccin theme built-in,
     # all the built-in themes are packaged into an extra package named `kitty-themes`
     # and it's installed by home-manager if `theme` is specified.
-    theme = "Catppuccin-Mocha";
+    themeFile = "Catppuccin-Mocha";
     font = {
       name = "JetBrainsMono Nerd Font";
       # use different font size on macOS
