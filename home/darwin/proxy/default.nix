28b20d279e1c6e1cfe915052d4a3d1adcbf5f629
Ryan Yin <xiaoyin_c@qq.com>
2024-11-28 10:06:22 +0800

    chore(home/darwin): proxy - mkOutOfStoreSymlink

--- home/darwin/proxy/default.nix
+++ home/darwin/proxy/default.nix
@@ -1,6 +1,13 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   home.packages = with pkgs; [
     clash-meta
   ];
-  home.file.".proxychains/proxychains.conf".source = ./proxychains.conf;
+
+  home.file.".proxychains/proxychains.conf".source =
+    config.lib.file.mkOutOfStoreSymlink
+    "${config.home.homeDirectory}/nix-config/home/darwin/proxy/proxychains.conf";
 }
