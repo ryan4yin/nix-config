0cac8e1763319b7cea8771b0321f4db18ca5872c
Ryan Yin <xiaoyin_c@qq.com>
2025-01-29 14:39:50 +0800

    feat: ghostty - use nushell

--- home/base/gui/terminal/ghostty.nix
+++ home/base/gui/terminal/ghostty.nix
@@ -29,6 +29,13 @@
       # only supported on macOS;
       background-blur-radius = 10;
       scrollback-limit = 20000;
+
+      # https://ghostty.org/docs/config/reference#command
+      #  To resolve issues:
+      #    1. https://github.com/ryan4yin/nix-config/issues/26
+      #    2. https://github.com/ryan4yin/nix-config/issues/8
+      #  Spawn a nushell in login mode via `bash`
+      command = "${pkgs.bash}/bin/bash --login -c 'nu --login --interactive'";
     };
   };
 }
