3ad2a334c94e784eeec5e5d6a015223245be3a97
Ryan Yin <xiaoyin_c@qq.com>
2025-01-29 13:51:30 +0800

    fix(NixOS): update lanzaboote

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.1";
+      url = "github:nix-community/lanzaboote/v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
