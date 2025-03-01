9d26022139576b5f5ee221206d796c40a9d095e5
Ryan Yin <xiaoyin_c@qq.com>
2025-03-01 08:47:35 +0800

    feat: enable remote desktop - sunshine

--- modules/nixos/desktop/remote-desktop/default.nix
+++ modules/nixos/desktop/remote-desktop/default.nix
@@ -3,11 +3,11 @@
   mylib,
   ...
 }: {
-  # imports = mylib.scanPaths ./.;
-  #
-  # environment.systemPackages = with pkgs; [
-  #   waypipe
-  #   moonlight-qt # moonlight client, for streaming games/desktop from a PC
-  #   rustdesk # p2p remote desktop
-  # ];
+  imports = mylib.scanPaths ./.;
+
+  environment.systemPackages = with pkgs; [
+    waypipe
+    moonlight-qt # moonlight client, for streaming games/desktop from a PC
+    rustdesk # p2p remote desktop
+  ];
 }
