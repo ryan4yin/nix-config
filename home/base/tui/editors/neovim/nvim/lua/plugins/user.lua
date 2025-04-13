2315c7c1df2d7432ee909945f4361dd6f13b22c1
Ryan Yin <xiaoyin_c@qq.com>
2025-04-13 16:11:18 +0800

    fix: hyprland v0.48.0 - breaking changes

--- home/base/tui/editors/neovim/nvim/lua/plugins/user.lua
+++ home/base/tui/editors/neovim/nvim/lua/plugins/user.lua
@@ -41,15 +41,15 @@ return {
   { "max397574/better-escape.nvim", enabled = false },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-    end,
-  },
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   config = function(plugin, opts)
+  --     require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+  --     -- add more custom luasnip configuration such as filetype extend or custom snippets
+  --     local luasnip = require "luasnip"
+  --     luasnip.filetype_extend("javascript", { "javascriptreact" })
+  --   end,
+  -- },
 
   {
     "windwp/nvim-autopairs",
