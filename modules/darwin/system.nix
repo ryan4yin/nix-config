19faab32f76bdfe4a337c7a2dd1c826960817834
Ryan Yin <xiaoyin_c@qq.com>
2025-05-21 12:44:33 +0800

    fix(darwin): nix-darwin - activation option & zsh option

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}:
+{pkgs, myvars, ...}:
 ###################################################################################
 #
 #  macOS's System configuration
@@ -22,12 +22,7 @@
   time.timeZone = "Asia/Shanghai";
 
   system = {
-    # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
-    activationScripts.postUserActivation.text = ''
-      # activateSettings -u will reload the settings from the database and apply them to the current session,
-      # so we do not need to logout and login again to make the changes take effect.
-      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-    '';
+  primaryUser = myvars.username;
 
     defaults = {
       menuExtraClock.Show24Hour = true; # show 24 hour clock
