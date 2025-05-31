fcb1f200ede130355b84d142e7a4e754be55cf36
Ryan Yin <xiaoyin_c@qq.com>
2025-06-01 01:39:34 +0800

    chore: remove duplicated config for foot terminal

--- home/base/gui/terminal/foot.nix
+++ home/base/gui/terminal/foot.nix
@@ -2,6 +2,16 @@
   programs.foot = {
     # foot is designed only for Linux
     enable = pkgs.stdenv.isLinux;
+
+    # foot can also be run in a server mode. In this mode, one process hosts multiple windows.
+    # All Wayland communication, VT parsing and rendering is done in the server process.
+    # New windows are opened by running footclient, which remains running until the terminal window is closed.
+    #
+    # Advantages to run foot in server mode including reduced memory footprint and startup time.
+    # The downside is a performance penalty. If one window is very busy with, for example, producing output,
+    # then other windows will suffer. Also, should the server process crash, all windows will be gone.
+    server.enable = true;
+
     # https://man.archlinux.org/man/foot.ini.5
     settings = {
       main = {
--- home/linux/gui/hyprland/values/wayland-apps.nix
+++ home/linux/gui/hyprland/values/wayland-apps.nix
@@ -3,24 +3,6 @@
   nur-ryan4yin,
   ...
 }: {
-  # refer to https://codeberg.org/dnkl/foot/src/branch/master/foot.ini
-  xdg.configFile."foot/foot.ini".text =
-    ''
-      [main]
-      dpi-aware=yes
-      font=JetBrainsMono Nerd Font:size=13
-      shell=${pkgs.bash}/bin/bash --login -c 'nu --login --interactive'
-      term=foot
-      initial-window-size-pixels=3840x2160
-      initial-window-mode=windowed
-      pad=0x0                             # optionally append 'center'
-      resize-delay-ms=10
-
-      [mouse]
-      hide-when-typing=yes
-    ''
-    + (builtins.readFile "${nur-ryan4yin.packages.${pkgs.system}.catppuccin-foot}/catppuccin-mocha.conf");
-
   home.packages = with pkgs; [
     # firefox-wayland
     nixpaks.firefox
@@ -28,19 +10,6 @@
   ];
 
   programs = {
-    # a wayland only terminal emulator
-    foot = {
-      enable = true;
-      # foot can also be run in a server mode. In this mode, one process hosts multiple windows.
-      # All Wayland communication, VT parsing and rendering is done in the server process.
-      # New windows are opened by running footclient, which remains running until the terminal window is closed.
-      #
-      # Advantages to run foot in server mode including reduced memory footprint and startup time.
-      # The downside is a performance penalty. If one window is very busy with, for example, producing output,
-      # then other windows will suffer. Also, should the server process crash, all windows will be gone.
-      server.enable = true;
-    };
-
     # source code: https://github.com/nix-community/home-manager/blob/master/modules/programs/chromium.nix
     google-chrome = {
       enable = true;
