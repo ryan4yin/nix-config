49796cdc508dd76b4c4026f7d39b2154c3e31e49
Ryan Yin <xiaoyin_c@qq.com>
2025-06-01 18:33:59 +0800

    feat: idols-ai - dual monitor with hdr enabled

--- hosts/idols-ai/home.nix
+++ hosts/idols-ai/home.nix
@@ -2,14 +2,9 @@
   modules.desktop = {
     hyprland = {
       nvidia = true;
-      settings = {
-        # Configure your Display resolution, offset, scale and Monitors here, use `hyprctl monitors` to get the info.
-        #   highres:      get the best possible resolution
-        #   auto:         position automatically
-        #   1.5:          scale to 1.5 times
-        #   bitdepth,10:  enable 10 bit support
-        monitor = "DP-2,highres,auto,1.5,bitdepth,10";
-      };
+      settings.source = [
+        "${config.home.homeDirectory}/nix-config/hosts/idols-ai/hypr-hardware.conf"
+      ];
     };
   };
 
