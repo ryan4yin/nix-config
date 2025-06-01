3818f9dabfc3d37f3d4ff1a0d2cfef15136acf06
Ryan Yin <xiaoyin_c@qq.com>
2025-06-01 19:19:58 +0800

    feat: gammastep - adjust temperature

--- home/linux/gui/base/eye-protection.nix
+++ home/linux/gui/base/eye-protection.nix
@@ -16,8 +16,8 @@
     # has problem with wayland, so disable it
     tray = false;
     temperature = {
-      day = 5700;
-      night = 4000;
+      day = 6000;
+      night = 4500;
     };
     # https://gitlab.com/chinstrap/gammastep/-/blob/master/gammastep.conf.sample?ref_type=heads
     settings = {
