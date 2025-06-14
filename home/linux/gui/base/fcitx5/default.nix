fb0f89d975221f330f2562b11f13faa0f659b79c
Ryan Yin <xiaoyin_c@qq.com>
2025-06-14 20:32:15 +0800

    feat: fcitx5 - wayland support

--- home/linux/gui/base/fcitx5/default.nix
+++ home/linux/gui/base/fcitx5/default.nix
@@ -18,6 +18,7 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
+    fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
       # for flypy chinese input method
       fcitx5-rime
