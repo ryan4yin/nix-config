a144093227c6bfd5891f7fc422bb0dabd531142c
Ryan Yin <xiaoyin_c@qq.com>
2025-07-01 16:20:52 +0800

    feat: allow to modify /etc/hosts for testing

--- modules/nixos/base/networking.nix
+++ modules/nixos/base/networking.nix
@@ -18,4 +18,8 @@
     "ntp.aliyun.com" # Aliyun NTP Server
     "ntp.tencent.com" # Tencent NTP Server
   ];
+
+  # dynamically update /etc/hosts for testing
+  # Note that changes made in this way will be discarded when switching configurations.
+  environment.etc.hosts.mode = "0644";
 }
