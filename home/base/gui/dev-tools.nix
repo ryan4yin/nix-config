99cd3f89365ee2eaa9e6980a1dea7bb443517e40
Ryan Yin <xiaoyin_c@qq.com>
2025-07-16 22:24:46 +0800

    chore: remove gemini, install it via npm instead

--- home/base/gui/dev-tools.nix
+++ home/base/gui/dev-tools.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  nur-ryan4yin,
-  ...
-}: {
+{pkgs, ...}: {
   home.packages = with pkgs;
     [
       mitmproxy # http/https proxy tool
@@ -12,7 +8,6 @@
       # jetbrains.idea-community
 
       # AI cli tools
-      nur-ryan4yin.packages.${pkgs.system}.gemini-cli
       k8sgpt
       kubectl-ai # an ai helper opensourced by google
     ]
