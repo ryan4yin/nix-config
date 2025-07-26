8c7da717caf3bc04a99da75ecc02cc932917ceb7
Ryan Yin <xiaoyin_c@qq.com>
2025-07-26 09:34:08 +0800

    feat: shoukei - add x64 support via binfmt

--- outputs/default.nix
+++ outputs/default.nix
@@ -26,6 +26,13 @@
         # To use chrome, we need to allow the installation of non-free software
         config.allowUnfree = true;
       };
+
+      pkgs-x64 = import nixpkgs {
+        system = "x86_64-linux";
+
+        # To use chrome, we need to allow the installation of non-free software
+        config.allowUnfree = true;
+      };
     };
 
   # This is the args for all the haumea modules in this folder.
