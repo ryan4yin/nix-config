329f03c1e2319dbef4fa3c7e0316378a84d7a3f4
Ryan Yin <xiaoyin_c@qq.com>
2025-08-17 17:23:17 +0800

    fix: remove duplicated values with catppuccin-nix

--- home/linux/gui/base/gtk.nix
+++ home/linux/gui/base/gtk.nix
@@ -38,10 +38,5 @@
     };
 
     gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
-
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
   };
 }
