c199f11582c45bee970a1a876db9293400ce14f3
Ryan Yin <xiaoyin_c@qq.com>
2025-08-20 20:20:40 +0800

    feat: niri - update window rules

--- home/linux/gui/niri/windowrules.nix
+++ home/linux/gui/niri/windowrules.nix
@@ -14,44 +14,31 @@ niri: {
       #   niri msg windows
 
       # --------------- Terminal ---------------
-      # foot → ws 13
       (plain "window-rule" [
         (leaf "match" { app-id = "foot"; })
         (leaf "open-on-workspace" "1terminal")
         (leaf "open-maximized" true)
       ])
 
-      # Alacritty → ws 10
       (plain "window-rule" [
         (leaf "match" { app-id = "Alacritty"; })
         (leaf "open-on-workspace" "1terminal")
         (leaf "open-maximized" true)
       ])
 
-      # Ghostty → ws 14
       (plain "window-rule" [
         (leaf "match" { app-id = "com.mitchellh.ghostty"; })
         (leaf "open-on-workspace" "1terminal")
         (leaf "open-maximized" true)
       ])
 
-      # --------------- Networking ---------------
-
-      # Clash Verge → ws 7
-      (plain "window-rule" [
-        (leaf "match" { app-id = "clash-verge"; })
-        (leaf "open-on-workspace" "0other")
-      ])
-
       # --------------- Browser ---------------
 
-      # Firefox → ws 11
       (plain "window-rule" [
         (leaf "match" { app-id = "firefox"; })
         (leaf "open-on-workspace" "2browser")
         (leaf "open-maximized" true)
       ])
-      # Google Chrome → ws 12
       (plain "window-rule" [
         (leaf "match" { app-id = "google-chrome"; })
         (leaf "open-on-workspace" "2browser")
@@ -59,10 +46,31 @@ niri: {
       ])
 
       # --------------- Chatting ---------------
-      # Telegram → ws 6
       (plain "window-rule" [
         (leaf "match" { app-id = "org.telegram.desktop"; })
         (leaf "open-on-workspace" "3chat")
+      ])
+      (plain "window-rule" [
+        (leaf "match" { app-id = "wechat"; })
+        (leaf "open-on-workspace" "3chat")
+      ])
+      (plain "window-rule" [
+        (leaf "match" { app-id = "QQ"; })
+        (leaf "open-on-workspace" "3chat")
+      ])
+
+      # --------------- Networking ---------------
+
+      (plain "window-rule" [
+        (leaf "match" { app-id = "clash-verge"; })
+        (leaf "open-on-workspace" "0other")
+      ])
+
+      # --------------- Meeting ---------------
+
+      (plain "window-rule" [
+        (leaf "match" { app-id = "Zoom Workplace"; })
+        (leaf "open-on-workspace" "0other")
       ])
     ];
 }
