74420b4161df0d3d5a90fc557f5ce3be2fdc3b68
Ryan Yin <xiaoyin_c@qq.com>
2025-10-18 19:55:02 +0800

    fix(nixos): hyprland - enable polkit-gnome to get auth work

--- home/linux/gui/hyprland/hyprland.nix
+++ home/linux/gui/hyprland/hyprland.nix
@@ -45,6 +45,7 @@ in
       variables = [ "--all" ];
     };
   };
+  services.polkit-gnome.enable = true; # polkit
 
   # NOTE: this executable is used by greetd to start a wayland session when system boot up
   # with such a vendor-no-locking script, we can switch to another wayland compositor without modifying greetd's config in NixOS module
