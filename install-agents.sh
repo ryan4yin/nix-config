c15e89e54f7c5592cb604d724f8b36b23e070406
Ryan Yin <xiaoyin_c@qq.com>
2026-03-10 23:16:03 +0800

    chore: commands for installing agents

---
+++ install-agents.sh
@@ -0,0 +1,15 @@
+# codex
+npm i -g @openai/codex
+
+# opencode
+npm install -g opencode-ai
+
+# kimi-cli
+uv tool install --python 3.13 kimi-cli
+uv tool upgrade kimi-cli --no-cache
+
+# gemini-cli
+npm install -g @google/gemini-cli
+
+# update all agents installed via npm
+npm update -g
