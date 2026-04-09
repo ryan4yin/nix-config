551020e0e890b643f63a6bee1dab404c4bf6d457
Ryan Yin <ryan4yin@linux.com>
2026-04-09 10:24:43 +0800

    feat: nixos - Screenshot Annotation via satty (#256)
    Signed-off-by: Ryan Yin <xiaoyin_c@qq.com>

--- home/linux/gui/niri/default.nix
+++ home/linux/gui/niri/default.nix
@@ -18,6 +18,11 @@ in
         home.packages = with pkgs; [
           # Niri v25.08 will create X11 sockets on disk, export $DISPLAY, and spawn `xwayland-satellite` on-demand when an X11 client connects
           xwayland-satellite
+
+          # for Screenshot Annotation
+          slurp
+          grim
+          satty
         ];
 
         xdg.configFile =
