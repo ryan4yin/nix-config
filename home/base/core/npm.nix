fb1d8118e3c7bdb6375a6ea68382729d0d4c324f
Ryan Yin <xiaoyin_c@qq.com>
2026-04-17 16:20:46 +0800

    feat: npmrc min-release-age

--- home/base/core/npm.nix
+++ home/base/core/npm.nix
@@ -1,10 +1,9 @@
 { config, ... }:
 {
-  # make `npm install -g <pkg>` happey
-  #
-  # mainly used to install npm packages that updates frequently
-  # such as opencode, codex, etc.
+  # 1. make `npm install -g <pkg>` happey
+  # 2. set min-release-age for security
   home.file.".npmrc".text = ''
     prefix=${config.home.homeDirectory}/.npm
+    min-release-age=7
   '';
 }
