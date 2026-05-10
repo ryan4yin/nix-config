26f4f86ad829af3e8dbcae74dd152fce399fb2b3
Ryan Yin <xiaoyin_c@qq.com>
2026-05-10 23:15:39 +0800

    chore: pypi - replace mirror server

--- home/base/core/pip.nix
+++ home/base/core/pip.nix
@@ -2,18 +2,6 @@ _: {
   # use mirror for pip install
   xdg.configFile."pip/pip.conf".text = ''
     [global]
-    index-url = https://mirror.nju.edu.cn/pypi/web/simple
-    format = columns
+    index-url = https://mirrors.bfsu.edu.cn/pypi/web/simple
   '';
-
-  # xdg.configFile."pip/pip.conf".text = ''
-  #   [global]
-  #   index-url = https://mirror.nju.edu.cn/pypi/web/simple
-  #   format = columns
-  # '';
-
-  # xdg.configFile."pip/pip.conf".text = ''
-  #   [global]
-  #   index-url = https://mirrors.bfsu.edu.cn/pypi/web/simple
-  # '';
 }
