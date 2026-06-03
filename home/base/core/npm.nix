92aca13c40e222a8dd2cc0d44db187bab0f77621
Ryan Yin <xiaoyin_c@qq.com>
2026-06-03 18:07:14 +0800

    chore: adjust npm min-release-age

--- home/base/core/npm.nix
+++ home/base/core/npm.nix
@@ -1,9 +1,9 @@
 { config, ... }:
 {
   # 1. make `npm install -g <pkg>` happey
-  # 2. set min-release-age for security
+  # 2. set min-release-age(in days) for security
   home.file.".npmrc".text = ''
     prefix=${config.home.homeDirectory}/.npm
-    min-release-age=7
+    min-release-age=2
   '';
 }
