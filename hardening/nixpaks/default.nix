21c76cf9cc47c2a51839f54446d47b4c7146c1b7
Ryan Yin <xiaoyin_c@qq.com>
2026-08-15 20:53:50 +0800

    fix(nixpaks): route qq via nixpkgs-master for 3.2.32-2026-07-30

--- hardening/nixpaks/default.nix
+++ hardening/nixpaks/default.nix
@@ -1,6 +1,6 @@
 {
   pkgs,
-  pkgs-patched,
+  pkgs-master,
   nixpak,
   ...
 }:
@@ -22,7 +22,7 @@ in
   nixpkgs.overlays = [
     (_: super: {
       nixpaks = {
-        qq = wrapper pkgs-patched ./qq.nix;
+        qq = wrapper pkgs-master ./qq.nix;
         telegram-desktop = wrapper super ./telegram-desktop.nix;
         firefox = wrapper super ./firefox.nix;
       };
