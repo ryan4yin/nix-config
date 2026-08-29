7921093de18cb5229ae190023b8cc37bb4c1842d
Ryan Yin <xiaoyin_c@qq.com>
2026-08-29 17:29:58 +0800

    fix(darwin): remove unused aerospace configuration

--- home/darwin/aerospace/default.nix
+++
@@ -1,5 +0,0 @@
-{ config, ... }:
-{
-  home.file.".aerospace.toml".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/home/darwin/aerospace/aerospace.toml";
-}
