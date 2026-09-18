e7f984745bf3c3d50dda610e74d0cce1ed86593e
Ryan Yin <xiaoyin_c@qq.com>
2026-09-18 15:58:04 +0800

    fix(home): autostart Chrome on aarch64 Linux

--- home/linux/gui/base/xdg/autostart.nix
+++ home/linux/gui/base/xdg/autostart.nix
@@ -22,11 +22,6 @@
     # nixpaks
     "${pkgs.nixpaks.firefox}/share/applications/org.mozilla.firefox.desktop"
     "${pkgs.nixpaks.telegram-desktop}/share/applications/org.telegram.desktop.desktop"
-  ]
-  ++ (
-    if pkgs.stdenv.hostPlatform.isx86_64 then
-      [ "${pkgs.google-chrome}/share/applications/google-chrome.desktop" ]
-    else
-      [ "${pkgs.chromium}/share/applications/chromium-browser.desktop" ]
-  );
+    "${pkgs.google-chrome}/share/applications/google-chrome.desktop"
+  ];
 }
